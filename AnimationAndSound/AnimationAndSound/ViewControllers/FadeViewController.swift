b5221be95cde8b6cb1f66e8fe7c50e61cb0dca86
cadanu <gdavidjoyce@gmail.com>
2023-02-21 10:18:05 -0500

    Layout and navigation complete

---
+++ AnimationAndSound/AnimationAndSound/ViewControllers/FadeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FadeViewController.swift
+//  AnimationAndSound
+//
+//  Created by GDJ on 2023-02-21.
+//
+
+import UIKit
+
+class FadeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AnimationAndSound/AnimationAndSound/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationAndSound
+//
+//  Created by GDJ on 2023-02-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToHome(sender: UIStoryboardSegue) {
+        
+    }
+    
+    
+
+
+}
+
